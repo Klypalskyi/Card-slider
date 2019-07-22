5fe3dc7a5d4b5fbb943e508ec56fcc1605781486
Клипальський Михайло <MKlypalskyi@bankgroup.pbank.if.ua>
2019-07-22 16:14:00 +0300

    some fixes

--- js/index.js
+++ js/index.js
@@ -1,11 +1,44 @@
-﻿document.addEventListener('DOMContentLoaded', function() {
-    var elems = document.querySelectorAll('.carousel');
-    var instances = M.Carousel.init(elems, options);
-  });
+﻿document.addEventListener("DOMContentLoaded", function() {
+  var elems = document.querySelectorAll(".carousel");
+  var instances = M.Carousel.init(elems, options);
+});
 
-  // Or with jQuery
+// Or with jQuery
 
-  $(document).ready(function(){
-    $('.carousel').carousel();
+$(document).ready(function() {
+  $(".carousel").carousel({
+    dist: -180,
+    numVisible: 7,
+    padding: -200
   });
-      
+});
+
+const form = document.querySelector(".value");
+const input = document.querySelector(".value-input");
+
+form.addEventListener("submit", submitForm);
+
+function submitForm(e) {}
+
+$(function() {
+  $("#test").swipe({
+    //Generic swipe handler for all directions
+    swipe: function(
+      event,
+      direction,
+      distance,
+      duration,
+      fingerCount,
+      fingerData
+    ) {
+      if (direction === "down") {
+        let summ = input.value;
+
+        if (summ !== '') {
+          alert(`Сумма ${summ} успішно переведена`);
+          form.reset();
+        }
+      } else return;
+    }
+  });
+});
